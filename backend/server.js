75b2dd0e8fb6694ec30dd4d40d5ad1dc7d5b961c
krushiler-official <krushilprajapati.er@gmail.com>
2026-03-28 18:23:50 +0530

    netlify changes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+
 import pgRoutes from "./routes/pgRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
@@ -9,17 +10,21 @@ import adminRoutes from "./routes/adminRoutes.js";
 
 // Load env variables
 dotenv.config();
+
 // Connect to MongoDB
 connectDB();
 
 const app = express();
+
 // ✅ CORS (IMPORTANT for Netlify frontend)
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "*",
+  origin: process.env.FRONTEND_URL || "https://pgfinder-us.netlify.app",// later you can replace with your Netlify URL
   credentials: true
 }));
+
 // Middleware
 app.use(express.json());
+
 // Routes
 app.use("/api/pgs", pgRoutes);
 app.use("/api/users", userRoutes);
@@ -30,6 +35,7 @@ app.use("/api/admin", adminRoutes);
 app.get("/", (req, res) => {
   res.send("Backend Running 🚀");
 });
+
 // Start server
 const PORT = process.env.PORT || 5000;
 
